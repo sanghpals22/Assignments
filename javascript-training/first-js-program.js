98b4f672623d23804b423c459f2306a735e451e4
Sanghpal Salve <sanghpal.s22@gmail.com>
2026-01-11 23:59:41 +0530

    initial commmit

---
+++ javascript-training/first-js-program.js
@@ -0,0 +1,13 @@
+console.log("Hello,World");
+console.info('Information');
+console.warn('Warning');
+console.error('Error');
+
+/* 
+console.log("Hello,World");
+console.info('Information');
+console.warn('Warning');
+console.error('Error'); 
+*/ 
+//  Shift+Alt+A to comment multiple lines
+//  Ctrl + / 
---
+++ typescript-training/first-ts-program.ts
@@ -0,0 +1,4 @@
+console.log("Hello,World");
+console.info('Information');
+console.warn('Warning');
+console.error('Error');
